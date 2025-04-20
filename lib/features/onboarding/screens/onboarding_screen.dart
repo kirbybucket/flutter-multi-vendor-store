a1c229f21ef4417fb178e6c7845bb359f86c7129
kirbybucket <haroldmoncadasoria@gmail.com>
2025-04-19 23:52:03 -0400

    chore(assets): add application visual assets
    - Include intro screen images (intro.png, intro1.png, intro3.png)
    
    - Add product showcase images (shoe2.jpg, shoe3.jpg, shoes1-2.jpg)
    
    - Implement user avatar placeholder (user_avatar.jpg)
    
    - Add social auth logos (facebook.png, google.png)

--- lib/features/onboarding/screens/onboarding_screen.dart
+++ lib/features/onboarding/screens/onboarding_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_vendor_store/features/onboarding/models/onboarding_item.dart';
+import 'package:get/get.dart';
+import 'package:flutter_multi_vendor_store/features/onboarding/controllers/onboarding_controller.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends GetView<OnboardingController> {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Onboarding Screen',
-        ),
+      body: Stack(
+        children: [
+          PageView.builder(
+              controller: controller.pageController,
+              itemCount: controller.items.length,
+              onPageChanged: controller.onPageChanged,
+              itemBuilder: (context, index) {
+                return _buildPage(
+                  context,
+                  controller.items[index],
+                );
+              }),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPage(BuildContext context, OnboardingItem item) {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            item.image,
+            height: 300,
+          ),
+          const SizedBox(height: 40),
+        ],
       ),
     );
   }
